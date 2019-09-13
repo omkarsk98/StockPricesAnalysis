f0929054ab38fc42092f2eeb24cc56c9d5b5cb44
Omkar <omkarsk98@gmail.com>
2019-09-13 11:41:00 +0530

    Candlestick graph created

--- queryFromMySQL.py
+++ queryFromMySQL.py
@@ -1,4 +1,6 @@
 import mysql.connector
+import plotly.graph_objects as go
+import pandas as pd
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -10,9 +12,17 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 mycursor.execute(
-    "select date, open, high, low, close, volume, (select open from hdfc h1 where h1.id>h.id order by id asc limit 1) as 'next day open' from hdfc h limit 10;")
+    "select date, open, high, low, close, volume, (select open from hdfc h1 where h1.id>h.id order by id asc limit 1) as 'next day open' from hdfc h;")
 
 myresult = mycursor.fetchall()
 
-for x in myresult:
-    print(x)
+df = pd.DataFrame(myresult, columns=[
+                  'date', 'open', 'high', 'low', 'close', 'volume', 'next day open'])
+# print(df)
+fig = go.Figure(data=[go.Candlestick(x=df['date'],
+                                     open=df['open'], high=df['high'],
+                                     low=df['low'], close=df['close'])
+                      ])
+
+fig.update_layout(xaxis_rangeslider_visible=False)
+fig.show()
